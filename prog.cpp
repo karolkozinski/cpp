a11ba8bf3727f70dd70df48e91e9858a750660ae
Karol Koziński <larzuck@gmail.com>
2024-09-09 14:51:06 +0200

    start again

---
+++ prog.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i {};
+        cout << i + 142 + 17 << "\n";
+        for (i = 1; i < 11; i++) {    
+        cout << i << ". Hello World 2" << "\n";
+   }
+    return 0;
+}
