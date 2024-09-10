8d70599be5ddbac20e21d1532d94e75bf296558d
Karol@Infnite <larzuck@gmail.com>
2024-09-10 17:50:51 +0200

    simple program

--- prog.cpp
+++ prog.cpp
@@ -7,7 +7,8 @@ int main()
     int x {};
     cout << "enter a number: ";
     cin >> x;
-    cout << "\n dublet " << x * 2;
+    cout << "\n dublet: " << x * 2;
+    cout << "\n triplet: " << x * 3;
     cout << endl;
     return 0;
 }
