74e12957cc3a9e075a7173906e71d392fcc8bf3e
Karol@Infnite <larzuck@gmail.com>
2024-09-10 19:01:58 +0200

    simple functions & string comparision

--- prog.cpp
+++ prog.cpp
@@ -1,14 +1,29 @@
+#include <string>
 #include <iostream>
-//#include <stdlib.h>
+
 using namespace std;
- 
+
+string co {};
+int liczba {};
+int cosZrob (string co, int liczba)
+{
+    if ( co == "podwoj" ) return liczba*2;
+    if ( co == "potroj" ) return liczba*3;
+    return 0;
+}
+
+
 int main()
 {
-    int x {};
-    cout << "enter a number: ";
-    cin >> x;
-    cout << "\n dublet: " << x * 2;
-    cout << "\n triplet: " << x * 3;
-    cout << endl;
+    do
+    {
+        cout << "Co robimy (podwoj/potroj): ";
+        cin >> co;
+    } while (co != "podwoj" && co != "potroj");
+
+    cout <<"Podaj liczbe: ";
+    cin >> liczba;
+    cout << cosZrob (co, liczba); 
+
     return 0;
 }
