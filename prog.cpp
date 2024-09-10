b009c1e1a33ce24309a8b4de382b02bca82c25c0
Karol@Infnite <larzuck@gmail.com>
2024-09-10 17:39:35 +0200

    dubel

--- prog.cpp
+++ prog.cpp
@@ -4,9 +4,10 @@ using namespace std;
  
 int main()
 {
-    //int x;
-    cout << 2 + 3;
-
+    int x {};
+    cout << "enter a number: ";
+    cin >> x;
+    cout << "\n dublet " << x * 2;
     cout << endl;
     return 0;
 }
